3310872f748a2bb28d63061e991a873d5c090f81
ArickCodeGuy <arickmail1997@gmail.com>
2023-04-08 13:25:40 +0300

    svelte-kit to svelte spa

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+import { Router, Route } from 'svelte-routing';
+import Layout from './routes/+layout.svelte';
+import Page from './routes/+page.svelte';
+</script>
+
+<Router basepath="/svelte-simple-mmo/">
+  <Layout>
+    <Route path="/"><Page /></Route>
+  </Layout>
+</Router>
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,26 @@
 import { resolve as pathResolve } from 'path';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
 
 const resolve = (path: string) => pathResolve(__dirname, path);
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [svelte()],
   base: '/svelte-simple-mmo/',
-
-  plugins: [sveltekit()],
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}'],
+  build: {
+    rollupOptions: {
+      // index for dev
+      // 404 for gh-pages
+      input: ['404.html', 'index.html'],
+    },
   },
   server: {
     port: 3000,
   },
   resolve: {
     alias: {
+      '@': resolve('src'),
       '~': resolve('src'),
     },
   },
