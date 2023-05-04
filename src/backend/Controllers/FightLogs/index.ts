d34e718baf85129208ccb163a4a68385f76dd598
ArickCodeGuy <arickmail1997@gmail.com>
2023-05-04 10:07:54 +0300

    small fixes

--- src/backend/Controllers/FightLogs/index.ts
+++ src/backend/Controllers/FightLogs/index.ts
@@ -1,13 +1,8 @@
 import { BaseController } from '../Base';
-import { usePushAction } from './actions/pushAction';
 import type { FightLog } from './types';
 
 export class FightLogController extends BaseController<FightLog> {
-  pushAction: ReturnType<typeof usePushAction>;
-
   constructor(tableName: string) {
     super(tableName);
-
-    this.pushAction = usePushAction(this);
   }
 }
