ef65b0407c353332fb943384234de7428ee2eefc
ArickCodeGuy <arickmail1997@gmail.com>
2023-07-24 09:21:01 +0300

    logs wip

--- src/routes/FightLogs/FightLogs.svelte
+++ src/routes/FightLogs/FightLogs.svelte
@@ -12,9 +12,43 @@ $: items = Server.publicApi.getFightLogs(globalInfo.living.id);
 <section class="section">
   <div class="container">
     <h1 class="h1">Fight Logs</h1>
-    {#each items as item}
-      <!--  -->
-    {/each}
-    <!-- items -->
+    <div class="fight-log-items">
+      {#each items as item}
+        <div class="fight-log-item">
+          <div class="fight-log-team-one">
+            {#each item.teamOne as member}
+              <div class="member">{member.id}</div>
+            {/each}
+          </div>
+          <div>VS</div>
+          <div class="fight-log-team-two">
+            {#each item.teamTwo as member}
+              <div class="member">{member.id}</div>
+            {/each}
+          </div>
+        </div>
+      {/each}
+    </div>
   </div>
 </section>
+
+<style lang="scss">
+.fight-log-items {
+  display: grid;
+  gap: var(--column-gutter);
+}
+.fight-log-item {
+  display: flex;
+  align-items: center;
+  gap: var(--column-gutter);
+}
+.member {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  border: 1px solid var(--contrast);
+}
+</style>
