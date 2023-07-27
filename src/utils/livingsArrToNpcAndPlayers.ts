223434dc5e71182a9d1c3a95461c3ab09dcff1f1
ArickCodeGuy <arickmail1997@gmail.com>
2023-07-27 08:59:16 +0300

    small fix

--- src/utils/livingsArrToNpcAndPlayers.ts
+++ src/utils/livingsArrToNpcAndPlayers.ts
@@ -4,7 +4,7 @@ import type { Living } from '@/backend/Controllers/Livings/types';
 export const livingsArrToNpcAndPlayers = (livingsArr: BaseItem<Living>[]) =>
   livingsArr.reduce<{ players: BaseItem<Living>[]; npc: BaseItem<Living>[] }>(
     (res, i) => {
-      if (i.protoId === 0) res.players.push(i);
+      if (i.protoId === 1) res.players.push(i);
       else res.npc.push(i);
       return res;
     },
