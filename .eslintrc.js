60a9e13e0787c958c816093f8baacf3a1bf9f08e
ArickCodeGuy <arickmail1997@gmail.com>
2023-08-05 10:02:26 +0300

    eslint, prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,30 @@
 module.exports = {
-  extends: [
-    // add more generic rule sets here, such as:
-    // 'eslint:recommended',
-    'plugin:svelte/recommended',
-  ],
-  rules: {
-    // override/add rules settings here, such as:
-    // 'svelte/rule-name': 'error'
-  },
+	root: true,
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier'
+	],
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	]
 };
